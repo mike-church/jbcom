efd2e6d34d9f317a366195069cc5b35897317496
Michael Church <michael@fishinglounge.com>
2017-02-17 11:28:57 -0500

    progress dev

--- wp-content/themes/sage-8.4.2/lib/setup.php
+++ wp-content/themes/sage-8.4.2/lib/setup.php
@@ -100,7 +100,7 @@ function display_sidebar() {
  * Theme assets
  */
 function assets() {
-  wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), '', '1.2');
+  wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), '', '1.3');
   wp_register_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:400,700,300,600', false, null);
   wp_enqueue_style('google-fonts');
 
