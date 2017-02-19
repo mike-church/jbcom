a2beaa3ff67a7eaedd8b20f83e64c1f57db27e93
Michael Church <michael@fishinglounge.com>
2017-02-19 11:55:19 -0500

    progress dev

--- wp-content/themes/sage-8.4.2/templates/head.php
+++ wp-content/themes/sage-8.4.2/templates/head.php
@@ -1,6 +1,16 @@
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php wp_head(); ?>
+<meta charset="utf-8">
+<meta http-equiv="x-ua-compatible" content="ie=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KMMWZRP');</script>
+<!-- End Google Tag Manager -->
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1411080325867149&ev=PageView&noscript=1"
+/></noscript>
+<?php wp_head(); ?>
 </head>
