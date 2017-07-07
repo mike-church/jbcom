e0f32103e0cd136c446456ad489dce9fa0593682
Michael Church <michael@fishinglounge.com>
2017-07-07 11:37:57 -0400

    progress dev

--- wp-content/themes/storefront/header.php
+++ wp-content/themes/storefront/header.php
@@ -14,28 +14,16 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-KMMWZRP');</script>
-<!-- End Google Tag Manager -->
-<noscript><img height="1" width="1" style="display:none"
-src="https://www.facebook.com/tr?id=1411080325867149&ev=PageView&noscript=1"
-/></noscript>
 
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-<!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KMMWZRP"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-<!-- End Google Tag Manager (noscript) -->
+
+<?php do_action( 'storefront_before_site' ); ?>
+
 <div id="page" class="hfeed site">
-	<?php
-	do_action( 'storefront_before_header' ); ?>
+	<?php do_action( 'storefront_before_header' ); ?>
 
 	<header id="masthead" class="site-header" role="banner" style="<?php storefront_header_styles(); ?>">
 		<div class="col-full">
