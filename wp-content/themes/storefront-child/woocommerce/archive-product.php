cf4bc8f1567265fed51c172ab3fc4b0ddbd113e6
Michael Church <michael@fishinglounge.com>
2017-08-06 14:40:18 -0400

    progress dev

---
+++ wp-content/themes/storefront-child/woocommerce/archive-product.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * The Template for displaying product archives, including the main shop page which is a post type archive
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/archive-product.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     2.0.0
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+get_header( 'shop' ); ?>
+
+	<?php if ( is_search() ){ ?>
+	<h3 class="margin-bottom-30"><?php /* Search Count */ $allsearch = &new WP_Query("s=$s&showposts=-1"); $key = wp_specialchars($s, 1); $count = $allsearch->post_count; _e(''); _e('<span class="search-terms">"'); echo $key; _e('"</span>');?> we found <?php echo $count . ' '; _e('products!'); wp_reset_query(); ?></h3>
+	<?php } ?>
+
+	<div class="filter-bar hidden-lg">
+		<h4>Narrow your choices</h4>
+		<ul class="filters">
+			<li><?php echo do_shortcode('[facetwp facet="diet_dropdown"]');?></li>
+			<li><?php echo do_shortcode('[facetwp facet="categories_dropdown"]');?></li>
+			<li><button class="button" onclick="FWP.reset()"><i class="fa fa-undo" aria-hidden="true"></i> Reset</button></li>
+		</ul>
+	</div>
+
+	<?php
+		/**
+		 * woocommerce_before_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper - 10 (outputs opening divs for the content)
+		 * @hooked woocommerce_breadcrumb - 20
+		 * @hooked WC_Structured_Data::generate_website_data() - 30
+		 */
+		do_action( 'woocommerce_before_main_content' );
+	?>
+
+
+
+<div class="margin-bottom-30 hidden-sm">
+<ul class="list-table">
+<li><?php echo do_shortcode('[facetwp counts="true"]') ;?></li>
+<li><?php echo do_shortcode('[facetwp sort="true"]') ;?></li>
+</ul>
+</div>
+
+
+<div class="facetwp-template">
+
+	<?php if ( have_posts() ) : ?>
+
+			<?php
+				/**
+				 * woocommerce_before_shop_loop hook.
+				 *
+				 * @hooked wc_print_notices - 10
+				 * @hooked woocommerce_result_count - 20
+				 * @hooked woocommerce_catalog_ordering - 30
+				 */
+				do_action( 'woocommerce_before_shop_loop' );
+			?>
+
+			<?php woocommerce_product_loop_start(); ?>
+
+				<?php woocommerce_product_subcategories(); ?>
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php
+						/**
+						 * woocommerce_shop_loop hook.
+						 *
+						 * @hooked WC_Structured_Data::generate_product_data() - 10
+						 */
+						do_action( 'woocommerce_shop_loop' );
+					?>
+
+					<?php wc_get_template_part( 'content', 'product' ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			<?php woocommerce_product_loop_end(); ?>
+
+			<?php
+				/**
+				 * woocommerce_after_shop_loop hook.
+				 *
+				 * @hooked woocommerce_pagination - 10
+				 */
+				do_action( 'woocommerce_after_shop_loop' );
+			?>
+
+		<?php elseif ( ! woocommerce_product_subcategories( array( 'before' => woocommerce_product_loop_start( false ), 'after' => woocommerce_product_loop_end( false ) ) ) ) : ?>
+
+			<?php
+				/**
+				 * woocommerce_no_products_found hook.
+				 *
+				 * @hooked wc_no_products_found - 10
+				 */
+				do_action( 'woocommerce_no_products_found' );
+			?>
+
+		<?php endif; ?>
+
+</div>
+
+<div class="hidden-sm" style="margin-top:30px">
+<ul class="list-table" >
+<li><?php echo do_shortcode('[facetwp counts="true"]') ;?></li>
+<li><?php echo do_shortcode('[facetwp per_page="true"]') ;?></li>
+</ul>
+</div>
+
+	<?php
+		/**
+		 * woocommerce_after_main_content hook.
+		 *
+		 * @hooked woocommerce_output_content_wrapper_end - 10 (outputs closing divs for the content)
+		 */
+		do_action( 'woocommerce_after_main_content' );
+	?>
+
+	<?php
+		/**
+		 * woocommerce_sidebar hook.
+		 *
+		 * @hooked woocommerce_get_sidebar - 10
+		 */
+		do_action( 'woocommerce_sidebar' );
+	?>
+
+<?php get_footer( 'shop' ); ?>
