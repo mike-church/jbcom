c203c8b11eeeb1eac8c3e073f82e6a47be8af65b
Michael Church <michael@fishinglounge.com>
2018-12-11 16:46:13 -0500

    updating

--- wp-content/themes/sage-8.4.2/template-paleo.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Template Name: Paleo
- */
-?>
-
-<?php while (have_posts()) : the_post(); ?>
-  <?php get_template_part('templates/content', 'paleo'); ?>
-<?php endwhile; ?>
--- wp-content/themes/sage-8.4.2/templates/content-page.php
+++
@@ -1,9 +0,0 @@
-<section class="section-padding">
-	<div class="container">
-		<div class="row">
-			<div class="col-sm-12">
-				<?php the_content(); ?>
-			</div>			
-		</div>
-	</div>
-</section>
