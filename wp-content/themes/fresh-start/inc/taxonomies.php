4064ab9749e8162f000775606c08b128da06874d
Michael Church <michaelchurch@Michaels-iMac-2.local>
2019-07-31 13:16:44 -0400

    add flavor taxonomy

--- wp-content/themes/fresh-start/inc/taxonomies.php
+++ wp-content/themes/fresh-start/inc/taxonomies.php
@@ -28,7 +28,35 @@ function fresh_start_diet() {
   );
 }
 
-add_action( 'init', 'fresh_start_product_line', 0 );
+add_action( 'init', 'fresh_start_flavor', 20 );
+function fresh_start_flavor() {
+  register_taxonomy(
+    'flavor',
+    array('product'),
+    array(
+      'labels' => array(
+        'name' => 'Flavor',
+        'menu_name' => 'Flavor',
+        'add_new_item' => 'Add New',
+        'new_item_name' => 'New Flavor'
+      ),
+      'show_ui' => true,
+      'show_tagcloud' => false,
+      'hierarchical' => true,
+      'sort' => true,      
+      'args' => array( 'orderby' => 'term_order' ),
+      'show_admin_column' => false,
+      'capabilities'=>array(
+        'manage_terms' => 'manage_options',
+        'edit_terms' => 'manage_options',
+        'delete_terms' => 'manage_options',
+        'assign_terms' =>'edit_posts'),
+      'rewrite' => array('with_front' => false, 'slug' => 'flavor'),
+    )
+  );
+}
+
+add_action( 'init', 'fresh_start_product_line', 30 );
 function fresh_start_product_line() {
   register_taxonomy(
     'product_line',
