ada962b0d1128774d324bfab344dda4e01221835
Michael Church <michaelchurch@Michaels-iMac-2.local>
2019-11-18 17:14:30 -0500

    updated to popup

--- wp-content/themes/fresh-start/inc/woocommerce/alerts.php
+++ wp-content/themes/fresh-start/inc/woocommerce/alerts.php
@@ -17,10 +17,19 @@ function wc_add_to_cart_message_filter($message, $product_id = null) {
     $titles = array_filter( $titles );
     $added_text = sprintf( _n( 'Successfully added to your cart.', 'Products have been added to your cart.', sizeof( $titles ), 'woocommerce' ), wc_format_list_of_items( $titles ) );
 
-		$message = sprintf( '%s <a href="%s">%s</a>&nbsp;|&nbsp;<a href="%s">%s</a>',
+		$message = sprintf( '%s <div class="container mt-4">
+            <div class="row">
+                <div class="col-sm-6">
+                    <a href="%s" class="btn btn-outline-primary btn-block mb-2 mb-sm-0">%s</a>
+                </div>
+                <div class="col-sm-6">
+                    <a href="%s" class="btn btn-primary btn-block text-white">%s</a>
+                </div>
+            </div>
+            </div>',
             esc_html( $added_text ),
             esc_url( wc_get_page_permalink( 'shop' ) ),
-            esc_html__( 'Continue Shopping', 'woocommerce' ),
+            esc_html__( 'Continue', 'woocommerce' ),
             esc_url( wc_get_page_permalink( 'cart' ) ),
             esc_html__( 'View Cart', 'woocommerce' ));
 
