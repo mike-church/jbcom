f9a30eaad8a1240ec50701eb9b6fcd019e19ad68
Michael Church <michaelchurch@Michaels-iMac-2.local>
2020-03-25 12:46:38 -0400

    update

--- wp-content/themes/fresh-start/inc/woocommerce/base.php
+++ wp-content/themes/fresh-start/inc/woocommerce/base.php
@@ -135,22 +135,3 @@ function zero_discount_for_excluded_products($discount, $discounting_amount, $ca
 }
 
 
-// Add FB Pixel conversion tracking code to the thank-you page
-add_action( 'woocommerce_thankyou', 'my_custom_tracking' );
-
-function my_custom_tracking( $order_id ) {
-
-  // Lets grab the order
-  $order = wc_get_order( $order_id );
-
-  ?>
-  <script>
-  fbq('track', 'Purchase', {
-  value: <?php echo $order->get_total(); ?>,
-  currency: 'USD'
-  });
-  </script>
-  <?php
-
-}
-
